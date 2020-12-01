55d1270f39d3b74293c9f6deecb4d263302a4070
dbrisco93 <dbrisco93@gmail.com>
2020-12-01 11:41:15 -0600

    commit tree 0644e159569fd046184ceb2e47d2b308e41a59ed

---
+++ index.js
@@ -0,0 +1,36 @@
+const wakeDog = function wakeDog(dogName, dogBreed) {
+    console.log(`Wake ${dogName} the ${dogBreed}`)
+}
+
+const leashDog = function leashDog(dogName, dogBreed) {
+    console.log(`Leash ${dogName} the ${dogBreed}`)
+}
+
+const walkToPark = function walkToPark(dogName, dogBreed) {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`)
+}
+
+const throwFrisbee = function throwFrisbee(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`)
+}
+
+const walkHome = function walkHome(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`)
+}
+
+const unleashDog = function unleashDog(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`)
+}
+
+
+
+const routine = [
+    wakeDog,
+    leashDog,
+    walkToPark,
+    throwFrisbee,
+    walkHome,
+    unleashDog
+]
+
+
